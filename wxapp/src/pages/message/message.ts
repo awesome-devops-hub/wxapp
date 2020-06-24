145b9c8fbd4152cb1e8110a4251ccb6bead98e49
Jiaoyang <yjiao@thoughtworks.com>
2020-06-24 18:37:32 +0800

    [Jiaoyang] refactor

--- wxapp/src/pages/message/message.ts
+++ wxapp/src/pages/message/message.ts
@@ -2,9 +2,13 @@ import { WxPage } from "../../core/wx/WxPage";
 import { pagify } from "../../core/utils/Utils";
 import { httpService } from "../../core/service/HttpService";
 import { IMessagePb, MessageRequest, MessageResponse } from "../../protocol/MessageProto";
-import { IPageablePb } from "../../protocol/ResourceProto";
+import { IPageablePb, IPagingPb } from "../../protocol/ResourceProto";
 
 interface State {
+  paging: IPagingPb;
+  triggered: boolean;
+  loading: boolean;
+  loadingMore: boolean;
   messages: IMessagePb[];
 }
 
@@ -37,19 +41,46 @@ class MessagePage extends WxPage<State> {
     };
   }
 
+  onRefresh() {
+    this.init();
+  }
+
   onReachEnd() {
-    console.log('reach end');
+    const { paging, messages, loadingMore } = this.data;
+    if (loadingMore || paging.totalCount <= messages.length) {
+      return;
+    }
+    this.setData({ loadingMore: true }, () => this.loadMore());
   }
 
   init() {
-    const pageable: IPageablePb = { page: 1, size: 10 } as IPageablePb;
+    this.setData({ loading: true });
+    const pageable: IPageablePb = { page: 1, size: 10 };
     return httpService.request(MessageRequest.create({ pageable }))
-      .subscribe((res: MessageResponse) => this.updateMessage(res.data));
+      .subscribe((res: MessageResponse) => this.updateMessage(res.data, res.paging));
   }
 
-  updateMessage(data: IMessagePb[]) {
-    this.setData({ messages: data });
-    const unreadCount = data?.filter(d => d.unread).length || null;
+  loadMore() {
+    const pageable: IPageablePb = {
+      ...this.data.paging,
+      page: this.data.paging.page,
+    };
+    return httpService.request(MessageRequest.create({ pageable }))
+      .subscribe((res: MessageResponse) => {
+        const messages = [...this.data.messages, ...res.data];
+        this.updateMessage(messages, res.paging);
+      });
+  }
+
+  updateMessage(messages: IMessagePb[], paging?: IPagingPb) {
+    this.setData({
+      messages,
+      paging,
+      triggered: false,
+      loading: false,
+      loadingMore: false,
+    });
+    const unreadCount = messages?.filter(d => d.unread).length || null;
     this.getTabBar().setData({ unreadCount });
   }
 }

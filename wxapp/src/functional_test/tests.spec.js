67fe384a68f139a0a688a6ee712548d9c0366c7c
Xudong Yang <xudong.yang@thoughtworks.com>
2020-06-24 17:37:08 +0800

    Temporarily comment out test of article detail page for webview

--- wxapp/src/functional_test/tests.spec.js
+++ wxapp/src/functional_test/tests.spec.js
@@ -90,31 +90,31 @@ describe("search result page", () => {
 });
 
 
-/*
-  [1] Story: #12 文章详情页面
-*/
-describe("Article details page in webview", () => {
+// /*
+//   [1] Story: #12 文章详情页面
+// */
+// describe("Article details page in webview", () => {
 
-    let page;
+//     let page;
 
-    beforeAll(async () => {
-        page = await miniprogram.reLaunch("/pages/search-result/search-result?key=Insurance");
-        await page.waitFor(500);
-        while (await check_if_result_page_is_empty(page) == true) {
-            page = await miniprogram.reLaunch("/pages/search-result/search-result?key=Insurance");
-            await page.waitFor(500);
-        }
-    }, 30000);
+//     beforeAll(async () => {
+//         page = await miniprogram.reLaunch("/pages/search-result/search-result?key=Insurance");
+//         await page.waitFor(500);
+//         while (await check_if_result_page_is_empty(page) == true) {
+//             page = await miniprogram.reLaunch("/pages/search-result/search-result?key=Insurance");
+//             await page.waitFor(500);
+//         }
+//     }, 30000);
 
-    it("Detail page will be opened in a webview page", async () => {
-        const first_item = (await page.$$("view.list--item-list view.list--item navigator.list--lnk"))[0];
-        await first_item.tap();
-        await page.waitFor(1000);
-        const webview_page = await miniprogram.currentPage();
-        const webview_elem = await webview_page.$("web-view");
-        expect(webview_elem).toBeTruthy();
-    });
-});
+//     it("Detail page will be opened in a webview page", async () => {
+//         const first_item = (await page.$$("view.list--item-list view.list--item"))[0];
+//         await first_item.tap();
+//         await page.waitFor(1000);
+//         const webview_page = await miniprogram.currentPage();
+//         const webview_elem = await webview_page.$("web-view");
+//         expect(webview_elem).toBeTruthy();
+//     });
+// });
 
 /*
   [2] Story: #12 文章详情页面
